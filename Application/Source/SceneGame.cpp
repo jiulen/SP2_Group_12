bb0982b8f32bf798eb6aa16389761ca592e6211e
PANG ZHI KAI <210513F@mymail.nyp.edu.sg>
2022-02-22 22:31:11 +0800

    lightpost code

--- Application/Source/SceneGame.cpp
+++ Application/Source/SceneGame.cpp
@@ -245,6 +245,7 @@ void SceneGame::Init()
 	meshList[GEO_SMALLHOUSE_F] = MeshBuilder::GenerateOBJMTL("small house F", "OBJ//small_buildingF.obj", "OBJ//small_buildingF.mtl");
 	meshList[GEO_WALL] = MeshBuilder::GenerateOBJMTL("wall", "OBJ//stoneWall.obj", "OBJ//stoneWall.mtl");
 	meshList[GEO_WALL_CORNER] = MeshBuilder::GenerateOBJMTL("wall", "OBJ//stoneWallCurve.obj", "OBJ//stoneWallCurve.mtl");
+	meshList[GEO_LIGHTPOST] = MeshBuilder::GenerateOBJMTL("light post", "OBJ//lightpostSingle.obj", "lightpostSingle.mtl");
 
 	//HUD
 	meshList[GEO_HEALTH] = MeshBuilder::GenerateQuad("health", Color(1, 1, 1), 1.f);
