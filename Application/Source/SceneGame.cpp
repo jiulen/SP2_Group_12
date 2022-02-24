23804394c6b66261cf781e86177f5211b5a53ac3
WappleLord <tanhongjun2714@gmail.com>
2022-02-24 10:25:05 +0800

    Tree fix

--- Application/Source/SceneGame.cpp
+++ Application/Source/SceneGame.cpp
@@ -217,7 +217,7 @@ void SceneGame::Init()
 	meshList[GEO_LIGHTPOST] = MeshBuilder::GenerateOBJMTL("light post", "OBJ//lightpostSingle.obj", "OBJ//lightpostSingle.mtl");
 	meshList[GEO_BENCH] = MeshBuilder::GenerateOBJMTL("bench", "OBJ//bench.obj", "OBJ//bench.mtl");
 	meshList[GEO_GROUND_GRASS] = MeshBuilder::GenerateOBJMTL("ground grass", "OBJ//ground_grass.obj", "OBJ//ground_grass.mtl");
-	meshList[GEO_TREE] = MeshBuilder::GenerateOBJMTL("tree", "OBJ//tree_oak.obj", "OBJ//tree_oak.mtl");
+	meshList[GEO_TREE] = MeshBuilder::GenerateOBJMTL("tree", "OBJ//tree.obj", "OBJ//tree.mtl");
 
 	//HUD + UI
 	meshList[GEO_BLUE] = MeshBuilder::GenerateQuad("blue", Color(0, 0.8, 1), 1.f);
